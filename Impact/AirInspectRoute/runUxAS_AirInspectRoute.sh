22d89187397dc08b3465dc74ad188ed8887d23f3
James Hamil <james.hamil.ctr@us.af.mil>
2017-06-21 11:03:23 -0400

    Add AirInspectRoute Impact example

---
+++ Impact/AirInspectRoute/runUxAS_AirInspectRoute.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../OpenUxAS/build/uxas"
+
+mkdir -p RUNDIR_AirInspectRoute
+cd RUNDIR_AirInspectRoute
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_AirInspectRoute.xml
