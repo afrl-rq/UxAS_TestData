309379d434c370f79adac910e5cc9df7f3cfb7bd
James Hamil <james.hamil.ctr@us.af.mil>
2017-06-21 12:50:49 -0400

    Add AirParallelSearchArea Impact example

---
+++ Impact/AirParallelSearchArea/runUxAS_AirParallelSearchArea.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../OpenUxAS/build/uxas"
+
+mkdir -p RUNDIR_AirParallelSearchArea
+cd RUNDIR_AirParallelSearchArea
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_AirParallelSearchArea.xml
