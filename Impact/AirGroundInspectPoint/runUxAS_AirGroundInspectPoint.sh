13002522f77899d7f72be48d21433c35190350f3
James Hamil <james.hamil.ctr@us.af.mil>
2017-06-21 11:02:23 -0400

    Add AirGroundInspectPoint Impact example

---
+++ Impact/AirGroundInspectPoint/runUxAS_AirGroundInspectPoint.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../OpenUxAS/build/uxas"
+
+mkdir -p RUNDIR_AirGroundInspectPoint
+cd RUNDIR_AirGroundInspectPoint
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_AirGroundInspectPoint.xml
