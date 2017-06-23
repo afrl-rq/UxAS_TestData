e0d14157b5fb813fea5f105098e4a6e8164f6afc
James Hamil <james.hamil.ctr@us.af.mil>
2017-06-23 15:49:03 -0400

    Add SurfaceInspectRoute Impact example

---
+++ Impact/SurfaceInspectRoute/runUxAS_SurfaceInspectRoute.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../OpenUxAS/build/uxas"
+
+mkdir -p RUNDIR_SurfaceInspectRoute
+cd RUNDIR_SurfaceInspectRoute
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_SurfaceInspectRoute.xml
