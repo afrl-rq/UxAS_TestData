502ad34377690d82b6e86aa2464593fe5f421037
James Hamil <james.hamil.ctr@us.af.mil>
2017-06-23 13:52:09 -0400

    Add MultiVehicleOverwatch Impact example

---
+++ Impact/MultiVehicleOverwatch/runUxAS_MultiVehicleOverwatch.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../OpenUxAS/build/uxas"
+
+mkdir -p RUNDIR_MultiVehicleOverwatch
+cd RUNDIR_MultiVehicleOverwatch
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_MultiVehicleOverwatch.xml
