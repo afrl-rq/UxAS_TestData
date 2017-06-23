4326f98e2a0b6175b6a9970c0db093ffd68ed028
James Hamil <james.hamil.ctr@us.af.mil>
2017-06-23 15:43:22 -0400

    Add SurfaceInspectPoint Impact example

---
+++ Impact/SurfaceInspectPoint/runUxAS_SurfaceInspectPoint.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../OpenUxAS/build/uxas"
+
+mkdir -p RUNDIR_SurfaceInspectPoint
+cd RUNDIR_SurfaceInspectPoint
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_SurfaceInspectPoint.xml
