24049f3ef130b20b321575ace8dec4683f5a0345
James Hamil <james.hamil.ctr@us.af.mil>
2017-06-23 13:39:21 -0400

    Add GroundOverwatch Impact example

---
+++ Impact/GroundOverwatch/runUxAS_GroundOverwatch.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../OpenUxAS/build/uxas"
+
+mkdir -p RUNDIR_GroundOverwatch
+cd RUNDIR_GroundOverwatch
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_GroundOverwatch.xml
